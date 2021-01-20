801557f52f6b6efa5abfb78f537cba47dbf9bb00
JuanMr <75852505+JuanMr7@users.noreply.github.com>
2021-01-20 14:51:11 -0500

    Update wordcloud.py

--- wordcloud.py
+++ wordcloud.py
@@ -1,2 +1,17 @@
 import re #Expresiones regulares
 from bs4 import BeatifulSoup #Es una librería que obtiene data de documentos de HTML y XML.
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+from wordcloud import WordCloud, ImageColorGenerator, STOPWORDS
+import requests 
+from io import BytesIO
+import sys, os, webbrowser, platform
+import pandas as pan 
+from os import path
+from matplotlib.pyplot import imread
+import time
+from sys import stdout
+from time import sleep
+
+
